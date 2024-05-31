d36ac96cd9c5c8fd1c4485b230be12b919319f5a
W Sanders <wsanders@wsanders.net>
2024-05-31 13:20:20 -0700

    Time is stored in UTC
    Fixed the rather silly issue that a resync to Wifi was required when changing to DST. The device still isn't aware when DST is in effect (and won't, ever), but it stores the time in UTC and doesn't have to be resynced when changing the TZ offset with Button B.

--- WSNixieClock.ino
+++ WSNixieClock.ino
@@ -1,5 +1,7 @@
 #include <M5StickCPlus.h>
+#include <Preferences.h>
 #include <WiFi.h>
+#include "images/header_pixel.h"
 #include "images/0.h"
 #include "images/1.h"
 #include "images/2.h"
@@ -18,18 +20,35 @@ int height = 60;
 int width = 60;
 char *data;
 const char* ntpServer = "us.pool.ntp.org";
-const long gmtOffset_sec     = 3600 * -8;
+long gmtOffset_sec     = 3600 * -8;
 const int daylightOffset_sec = 0;
 
 //OBFUSCATE!
-const char* ssid     = "YOURSSID";
-const char* password = "YOURPASS";
+const char* ssid     = "YOUR SSID";
+const char* password = "YOUR PASSWORD";
 struct tm RTCtime;
-
+int bright =  0;
+int TZoffset = 0;
+long starttime; 
+Preferences preferences;
 
 void setup() { 
+    // the order of the next two statements does not affect the startup flash
     M5.begin();
+    M5.Lcd.fillScreen(BLACK);
+    // Get preferences from EEPROM - this is preferred interface than EEPROM.h
+    // M5StickC-Plus/examples/Advanced/Storage/Counter for example
+    // second param is "readonly"
+    preferences.begin("NixieClock", false);
+    bright = preferences.getInt( "bright", bright);
+    // int +-1 hours
+    TZoffset = preferences.getInt( "TZoffset", TZoffset);
     M5.Lcd.setRotation(3);
+    M5.Axp.ScreenBreath(bright);
+    // Seems to be included with base device .h, set to 115200
+    Serial.println("works");
+
+    // out of sheer laziness copy header images to bitmaps
     for (int i=0; i<10; i++) { 
         bitmap[i] = (uint16_t *)malloc(height * width * sizeof(uint16_t));
     }
@@ -52,14 +71,16 @@ void setup() {
             }
         }
     }
-    setRTCTime();
+
+    // timeout
+    starttime = millis();
 }
 
  
 void loop() {
         int d1,d2,d3,d4;
-
         getRTCTime(); 
+        RTCtime.tm_hour = (RTCtime.tm_hour + TZoffset) % 24;
         d1 = RTCtime.tm_hour / 10;        
         d2 = RTCtime.tm_hour % 10;
         d3 = RTCtime.tm_min / 10;        
@@ -68,7 +89,39 @@ void loop() {
         M5.Lcd.drawBitmap(60 ,135/2-30,60,60,bitmap[d2]);
         M5.Lcd.drawBitmap(120 ,135/2-30,60,60,bitmap[d3]);
         M5.Lcd.drawBitmap(180 ,135/2-30,60,60,bitmap[d4]);
-        delay(800);
+        M5.update();
+        if (M5.BtnA.wasPressed() && M5.BtnB.wasPressed()) { 
+            setRTCTime();
+        } else if (M5.BtnA.wasPressed()) {
+            bright++;
+            if (bright > 13) { 
+                bright = 7;
+            }
+            M5.Axp.ScreenBreath(bright);
+            M5.Lcd.setCursor(2,120);
+            M5.Lcd.print("SET BRIGHT: ");
+            M5.Lcd.println(bright);
+        } else if (M5.BtnB.wasPressed()) {
+            // cycle through some TZoffsets
+            TZoffset--;
+            if (TZoffset < -10) { TZoffset = 0; }
+            M5.Lcd.setCursor(0,120);
+            M5.Lcd.print("TZ offset: ");
+            M5.Lcd.print(TZoffset);
+        } else {
+            //M5.Lcd.setCursor(0,120);
+            ////M5.Lcd.println("                ");
+        }
+        battStat();
+        delay(200);
+        if (millis() > (starttime + 20000) && M5.Axp.GetIdischargeData() > 0.0)
+        { 
+            // save brightness, tzoffset in prefs
+            preferences.putInt("bright", bright);
+            preferences.putInt("TZoffset", TZoffset);
+            preferences.end();
+            M5.Axp.PowerOff();
+        }
 }
 
 uint16_t rgb888to565 (int r, int g, int b) {
@@ -80,10 +133,14 @@ uint16_t rgb888to565 (int r, int g, int b) {
 
 
 void setRTCTime() {
+    // REWRITE: Prompt for SSID, password, then set RTC to UTC
     RTC_TimeTypeDef RTC_TimeStruct;
     RTC_DateTypeDef RTC_DateStruct;
     struct tm timeinfo;
 
+    M5.Lcd.setCursor(0,0);
+    M5.Lcd.println("Connect USB to serial at 115200...");
+    M5.Lcd.println("Type CR in the console when ready...");
     WiFi.mode(WIFI_STA);
     M5.Lcd.printf("\nConnecting to %s", ssid);
     WiFi.begin(ssid, password);  // Connect wifi and return connection status.
@@ -92,9 +149,11 @@ void setRTCTime() {
         M5.Lcd.print(".");
     }
     M5.Lcd.println("\nCONNECTED!");
+    gmtOffset_sec = 0;
     configTime(gmtOffset_sec, daylightOffset_sec, ntpServer); //  get the time
     // tm_sec,tm_min,tm_hr,tm_mday,tm_wday,tm_mon,tm_year,tm_yday,tm_isdst
     // Unfortunate name, we are really getting NTP time
+    // storing local time for now out of sheer laziness
     if (!getLocalTime(&timeinfo)) {
         M5.Lcd.println("Failed to obtain time");
         return;
@@ -109,7 +168,6 @@ void setRTCTime() {
     M5.Rtc.SetTime(&RTC_TimeStruct);
     M5.Rtc.SetDate(&RTC_DateStruct);
     // I have no idea why this works:
-    M5.Lcd.setCursor(0,0);
     M5.Lcd.println(&timeinfo, "RTC SET: %A, %B %d \n%Y %H:%M:%S");
     WiFi.disconnect(true);
     WiFi.mode(WIFI_OFF);
@@ -133,3 +191,27 @@ void getRTCTime() {
     //M5.Lcd.println(&RTCtime, "RTC TIME IS : %A, %B %d \n%Y %H:%M:%S");
 }
 
+void battStat() {
+    int xorigin = 200;
+    int yorigin = 1;
+    int c = GREEN;
+    float batVoltage = M5.Axp.GetBatVoltage();
+    // from Core2_factory_test.ino
+    float batPct = ( batVoltage < 3.2 ) ? 0 : ( batVoltage - 3.2 ) * 100.;
+    if (batPct < 5.0) { 
+        c = RED;
+    }
+    M5.Lcd.drawRect(xorigin, yorigin,30,15, c);
+    M5.Lcd.drawRect(xorigin+29, yorigin+4,4,7,c);
+    if (batPct > 25.0) {
+        M5.Lcd.fillRect(xorigin+2, yorigin+2,8,11,GREEN);
+    }
+    if (batPct > 50.0) {
+        M5.Lcd.fillRect(xorigin+11, yorigin+2,8,11,GREEN);
+    }
+    if (batPct > 75.0) {
+        M5.Lcd.fillRect(xorigin+20, yorigin+2,8,11,GREEN);
+    }
+    
+    
+}
